e0cbbb074d756be86fb3e293aad72dbb190b0d67
Hlungwani Malwandla Shaun <hlungwanimalwandlashaun@gmail.com>
2022-12-08 18:49:54 -0800

    again

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
 /**
- * main - prints the size of various types
- * Return: 0 if exited properly, non zero otherwise
+ * main - prints the size of various types,
+ * but the outcome is a piece of art,
+ * Return: 0 if exited properly, non zero otherwise,
  */
 
 int main(void)
