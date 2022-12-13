abbfae138afe32530b9efb2c49c117e6944407a8
Hlungwani Malwandla Shaun <hlungwanimalwandlashaun@gmail.com>
2022-12-13 06:44:09 -0800

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -2,7 +2,7 @@
 #include <math.h>
 
 /**
- * main - prints out text 
+ * main -  prints _putchar, followed by a new line
  * printf - is a gunction that prints out to the terminal
  * int - is data type
  */
