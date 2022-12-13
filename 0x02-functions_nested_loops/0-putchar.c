98eef14153995db1c234701c03d9a4be8ea47745
Hlungwani Malwandla Shaun <hlungwanimalwandlashaun@gmail.com>
2022-12-13 06:22:53 -0800

    first commit

---
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+
+/**main - is the entry point function
+ * printf - is a gunction that prints out to the terminal
+ * int - is data type
+ */
+int main(void)
+{
+	print("_puchar\n");
+}
