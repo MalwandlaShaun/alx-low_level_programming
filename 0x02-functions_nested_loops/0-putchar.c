29cdeb1a47d898ff385c39cf69b9b6c8a963bca3
Hlungwani Malwandla Shaun <hlungwanimalwandlashaun@gmail.com>
2022-12-13 06:46:11 -0800

    yeah

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,6 +5,7 @@
  * main -  prints _putchar, followed by a new line
  * printf - is a gunction that prints out to the terminal
  * int - is data type
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
